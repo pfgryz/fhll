a67576c14a9185e5b0b74a24f2aea4758d8c4034
pgryz <01169171@pw.edu.pl>
2024-06-09 14:33:20 +0200

    Add tests to functions collector

---
+++ tests/interpreter/visitors/test_functions_collector.py
@@ -0,0 +1,54 @@
+import pytest
+
+from src.interpreter.errors import ParameterRedeclarationError
+from src.interpreter.types.typename import TypeName
+from src.interpreter.visitors.functions_collector import FunctionsCollector
+from src.interpreter.visitors.types_collector import TypesCollector
+from tests.interpreter.visitors.helpers import load_module
+
+
+def test_functions_collector__simple():
+    module = load_module("""
+    fn main(x: i32, mut y: i32) -> i32 {
+        return x * x;
+    }
+    
+    fn boot() {
+        main(3, 4);
+    }
+    """)
+
+    types_collector = TypesCollector()
+    types_collector.visit(module)
+    types_registry = types_collector.types_registry
+
+    functions_collector = FunctionsCollector(types_registry)
+    functions_collector.visit(module)
+    functions_registry = functions_collector.functions_registry
+
+    boot = functions_registry.get_function("boot")
+    main = functions_registry.get_function("main")
+    assert boot is not None
+    assert boot.return_type is None
+    assert main is not None
+    assert main.name == "main"
+    assert main.return_type == TypeName.parse("i32")
+    assert main.parameters.get("x") == (False, TypeName.parse("i32"))
+    assert main.parameters.get("y") == (True, TypeName.parse("i32"))
+
+
+def test_functions_collector__parameter_redeclaration():
+    module = load_module("""
+    fn main(x: i32, mut x: i32) -> i32 {
+        return x * x;
+    }
+    """)
+
+    types_collector = TypesCollector()
+    types_collector.visit(module)
+    types_registry = types_collector.types_registry
+
+    functions_collector = FunctionsCollector(types_registry)
+
+    with pytest.raises(ParameterRedeclarationError):
+        functions_collector.visit(module)
